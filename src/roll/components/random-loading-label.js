153a05f8c533f52a8293db04d4168d05c3396cde
Snirk Immington <snirk.immington@gmail.com>
2017-11-25 18:07:32 -0500

    Update random button flavortext to indicate loading failed

--- src/roll/components/random-loading-label.js
+++ src/roll/components/random-loading-label.js
@@ -3,8 +3,13 @@
 import './random-loading-label.css';
 
 import React, { Component } from 'react';
-import { ControlLabel } from 'react-bootstrap';
+import { ControlLabel, Button } from 'react-bootstrap';
 
+import { connect } from 'react-redux';
+
+import type { LoadingState } from '../state';
+import type { AppState, DispatchFn, GetStateFn } from '../../state';
+import * as rollActions from '../actions';
 import pickRandom from '../../util/pick-random';
 
 const loadingFlavorText: string[] = [
@@ -52,17 +57,77 @@ const loadedFlavorText: string[] = [
     "Go get 'em, chummer",
 ];
 
-type LoadingProps = { isLoading: boolean };
+const localRequiredFlavorText: string[] = [
+    "Can't access random.org",
+    "random.org is offline",
+    "Can't find random.org on the Matrix",
+    "Too much noise to access random.org",
+    "Unable to find random.org's host",
+];
 
-export default class RandomLoadingLabel extends Component<LoadingProps> {
+const localLoadingFlavorText: string[] = [
+    "Getting randoness from your browser",
+    "Hope your browser has good RNG",
+    "Your browser better be random enough, chummer",
+];
 
-    shouldComponentUpdate(nextProps: LoadingProps) {
-        return nextProps.isLoading !== this.props.isLoading;
+const localLoadedFlavorText: string[] = [
+    "Rolls from Random.next()",
+    "Rolls from your browser",
+    "Pseudorandom rolls from your browser",
+    "Pseudorandom rolls from your browser",
+    "Pseudorandom rolls from your browser",
+    "Possibly-compromised rolls from your browser",
+
+    "Rolling in offline mode",
+    "Rolling without wireless bonus",
+];
+
+type Props = {|
+    dispatch: DispatchFn,
+    getState: GetStateFn,
+
+    bufferLoadState: LoadingState,
+    isLocal: boolean,
+|};
+
+class RandomLoadingLabel extends Component<Props> {
+    shouldComponentUpdate(nextProps: Props) {
+        return nextProps.bufferLoadState !== this.props.bufferLoadState
+            || nextProps.isLocal !== this.props.isLocal;
+    }
+
+    handleFillLocal = (event: SyntheticInputEvent<HTMLButtonElement>) => {
+        this.props.dispatch(rollActions.bufferSetLocal(true));
+        this.props.dispatch(rollActions.fetchBuffer());
+    }
+
+    handleGoWireless = (event: SyntheticInputEvent<HTMLButtonElement>) => {
+        this.props.dispatch(rollActions.clearBuffer());
+        this.props.dispatch(rollActions.bufferSetLocal(false));
+        this.props.dispatch(rollActions.fetchBuffer());
     }
 
     render() {
-        if (this.props.isLoading) {
-            const flavorText = pickRandom(loadingFlavorText);
+        if (this.props.bufferLoadState === "failed") {
+            const flavorText = pickRandom(localRequiredFlavorText);
+            return (
+                <span className="loading-label-with-link roll-loading-label">
+                    <ControlLabel className="roll-menu-label"
+                                  controlId="roll-input-local-buffer">
+                        {flavorText}
+                    </ControlLabel>
+                    {" ("}
+                    <a href="#roll-input-panel"
+                       onClick={this.handleFillLocal}>
+                        Use local RNG
+                    </a>)
+                </span>
+            );
+        }
+        else if (this.props.bufferLoadState === "loading") {
+            const flavorText = this.props.isLocal ?
+             pickRandom(localLoadingFlavorText) : pickRandom(loadingFlavorText);
             return (
                 <ControlLabel className="roll-loading-label roll-menu-label">
                     <span className="dice-roll-icon roll-menu-icon" />
@@ -71,12 +136,39 @@ export default class RandomLoadingLabel extends Component<LoadingProps> {
             );
         }
         else {
-            const flavorText = pickRandom(loadedFlavorText);
-            return (
-                <ControlLabel className="roll-loading-label">
-                    <i>{flavorText}</i>
-                </ControlLabel>
-            );
+            if (this.props.isLocal) {
+                const flavorText = pickRandom(localLoadedFlavorText);
+                return (
+                    <span className="roll-loading-label">
+                        <ControlLabel className="roll-menu-label"
+                                      controlId="roll-input-random-buffer">
+                            {flavorText}
+                        </ControlLabel>
+                        {" ("}
+                        <a href="#roll-input-panel"
+                           onClick={this.handleGoWireless}>
+                            Use random.org
+                        </a>)
+                    </span>
+                )
+            }
+            else {
+                const flavorText = pickRandom(loadedFlavorText);
+                return (
+                    <ControlLabel className="roll-loading-label roll-menu-label">
+                        {flavorText}
+                    </ControlLabel>
+                );
+            }
         }
     }
 }
+
+function mapStateToProps(state: AppState) {
+    return {
+        bufferLoadState: state.roll.bufferLoadState || "loading",
+        isLocal: state.roll.bufferIsLocal || false,
+    };
+}
+
+export default connect(mapStateToProps)(RandomLoadingLabel);
