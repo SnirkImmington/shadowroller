5a44c310d694c934ffce5b3bd4b535428dc3c255
Snirk Immington <snirk.immington@gmail.com>
2017-11-30 13:02:47 -0500

    Add 'favortext' (forever typo) component

---
+++ src/components/favortext.js
@@ -0,0 +1,30 @@
+// @flow
+
+import * as React from 'react';
+
+import pickRandom from '../util/pick-random';
+
+type Props = {
+    from: React.Node[]
+};
+
+/**
+Picks some flavor text from a list.
+
+More importantly, doesn't change flavor text every time React goes to
+re-render it.
+
+The list MUST be a single-reference const (do not construct lists on the fly).
+*/
+export default class FavorText extends React.Component<Props> {
+    shouldComponentUpdate(nextProps: Props) {
+        // So we could just give a flat "NO" but this is cheaper.
+        return nextProps.from !== this.props.from;
+    }
+
+    render() {
+        const text = pickRandom(this.props.from);
+
+        return <span className="favortext">{text}</span>;
+    }
+}
