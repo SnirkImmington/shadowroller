009a57e87122c9be6cdacc8ac8289619d4386450
Snirk Immington <snirk.immington@gmail.com>
2017-12-13 18:35:38 -0500

    Try and center the input boxes on mobile

--- src/roll/containers/options/test-for.js
+++ src/roll/containers/options/test-for.js
@@ -15,7 +15,7 @@ type Props = {
 export default function TestForRollOptions(props: Props) {
     return (
         <FormGroup controlId="roll-input-test-for"
-                   className="roll-input-options">
+                   className="roll-input-options roll-option-offset">
             <NumericInput controlId="roll-input-test-for"
                 onSelect={props.onChange} />
             <ControlLabel className="menu-label">
