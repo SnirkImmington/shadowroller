ff727729b6a2545b626b42d9804ac5910d2152f9
S. B. Immington <snirk.immington@gmail.com>
2020-03-29 17:28:54 -0400

    Move config to its own module for now

---
+++ config/config.go
@@ -0,0 +1,51 @@
+package config
+
+import (
+	"os"
+	"strconv"
+)
+
+var (
+	IsProduction = readBool("IS_PRODUCTION", false)
+	// Server configs
+	ServerAddress   = readString("SERVER_ADDRESS", ":8921")
+	FrontendAddress = readString("FRONTEND_ADDRESS", "localhost:3000")
+	// TLS configs
+	TlsEnable = readBool("TLS_ENABLE", false)
+	TlsHost   = readString("TLS_HOST", "https://shadowroller.immington.industries")
+	CertDir   = readString("CERT_DIR", "/var/sr-server/cert/")
+	// Timeouts
+	ReadTimeoutSecs     = readInt("READ_TIMEOUT_SECS", 30)
+	WriteTimeoutSecs    = readInt("WRITE_TIMEOUT_SECS", 30)
+	IdleTimeoutSecs     = readInt("IDLE_TIMEOUT_SECS", 60)
+	SSEWriteTimeoutSecs = readInt("SSE_WRITE_TIMEOUT_SECS", 30)
+	MaxHeaderBytes      = readInt("MAX_HEADER_BYTES", 1<<20)
+	// LibraryOptions
+	RedisUrl = readString("REDIS_URL", "redis://redis:6379")
+	// Backend options
+	HardcodedGameNames = readString("GAME_NAMES", "testgame1,testgame2")
+)
+
+func readString(name string, defaultValue string) string {
+	val, ok := os.LookupEnv("SR_" + name)
+	if !ok {
+		return defaultValue
+	}
+	return val
+}
+
+func readInt(name string, defaultValue int) int {
+	val, err := strconv.Atoi(name)
+	if err != nil {
+		return defaultValue
+	}
+	return val
+}
+
+func readBool(name string, defaultValue bool) bool {
+	val, err := strconv.ParseBool(name)
+	if err != nil {
+		return defaultValue
+	}
+	return val
+}
