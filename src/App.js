cf5d4c077469742e0e1f8514895e21262fb7ca68
S. B. Immington <shadowroller@immington.industries>
2020-03-31 21:19:59 -0400

    Experiment with styled-components

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ import React from 'react';
 import RollInputPanel from './roll/containers/roll-input-panel';
 import RollHistoryPanel from './roll/containers/roll-history-panel';
 
+import styled from 'styled-components/macro';
+
 //* Main page component. */
-export default function App(props: {}) {
+export function App(props: {}) {
     return (
         <div className="App rounded-0">
             <header className="App-header mb-2">
@@ -19,5 +21,37 @@ export default function App(props: {}) {
                 <RollHistoryPanel />
             </div>
         </div>
+    );
+}
+
+const Page = styled.div`
+    border-radius: 0;
+`;
+
+const AppHeader = styled.header`
+    background-color: #222;
+    height: 4em;
+    padding: 0.9em;
+    color: white;
+`;
+
+const AppTitle = styled.h1`
+    font-size: 2em;
+    font-style: oblique;
+    font_weight: 900;
+    text-align: center;
+`;
+
+export default function App2(props: {}) {
+    return (
+        <Page>
+            <AppHeader className="mb-2">
+                <AppTitle>Shadowroller</AppTitle>
+            </AppHeader>
+            <div className="App-wide-container">
+                <RollInputPanel />
+                <RollHistoryPanel />
+            </div>
+        </Page>
     );
 }
