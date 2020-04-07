8b65668bc9e3a15aca00acb003db2e4c8fe43787
S. B. Immington <shadowroller@immington.industries>
2020-04-07 19:54:44 -0400

    Begin moving request code middleware around

--- request.go
+++ request.go
@@ -3,17 +3,13 @@ package srserver
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/gomodule/redigo/redis"
 	"log"
 	"net/http"
 	"srserver/config"
 	"time"
 )
-
-//responseLogger := Logger
-
-type HandlerFunc = func(http.ResponseWriter, *http.Request) // TODO add params
-type RedisHandlerFunc = func(http.ResponseWriter, *http.Request, redis.Conn)
 
 var redisPool = &redis.Pool{
 	MaxIdle:     10,
@@ -23,37 +19,51 @@ var redisPool = &redis.Pool{
 	},
 }
 
-func makeRequestHandler(wrapped HandlerFunc) http.HandlerFunc {
-	return func(response http.ResponseWriter, request *http.Request) {
-		// Timing?
-		if config.IsProduction {
-			log.Println(request.Proto, request.Method, request.RequestURI)
-		} else {
-			log.Println(request.Method, request.URL)
-		}
-		defer func() {
-			if err := recover(); err != nil {
-				log.Println("Panic serving", request.Method, request.URL, "to", request.Host)
-				// TODO print stack trace (minus the wrapper of this fn + makeRequestHandler)
-				log.Println("staaaaaaaaaaaaaaaack traaaaaaaaaaaaaaaaaaaace")
-				http.Error(response, "Internal Server Error", http.StatusInternalServerError)
-			}
-		}()
-		wrapped(response, request /* */)
-		// We could return a value here to be logged
-		//log.Println("Response:", response.Status)
+func readBodyJSON(request *Request, value interface{}) error {
+	decoder := json.NewDecoder(request.Body)
+	return decoder.Decode(value)
+}
+
+func writeBodyJSON(response Response, value interface{}) error {
+	return json.NewEncoder(response).Encode(value)
+}
+
+func httpUnauthorized(response Response) {
+	log.Print("->", http.StatusUnauthorized, "Unauthorized")
+	http.Error(response, "Unauthorized", http.StatusUnauthorized)
+}
+
+func httpInternalError(response Response, request *Request, err error) {
+	logMessage := fmt.Sprintf("Internal error handling %s %s: %w", request.Method, request.URL, err)
+	log.Output(2, logMessage)
+	if config.IsProduction {
+		log.Print("-> 500 Internal Error")
+		http.Error(response, "Internal Server Error", http.StatusInternalServerError)
+	} else {
+		log.Printf("-> 500 %w", err)
+		http.Error(response, logMessage, http.StatusInternalServerError)
 	}
 }
 
-func makeRedisRequestHandler(wrapped RedisHandlerFunc) http.HandlerFunc {
-	return makeRequestHandler(func(response http.ResponseWriter, request *http.Request) {
-		redisConn := redisPool.Get()
-		defer redisConn.Close()
-		wrapped(response, request, redisConn)
-	})
+func httpInternalErrorMessage(response Response, request *Request, message interface{}) {
+	logMessage := fmt.Sprintf("Internal error handling %s %s: %v", request.Method, request.URL, message)
+	log.Output(2, logMessage)
+	if config.IsProduction {
+		log.Print("-> 500 Internal Error")
+		http.Error(response, "Internal Server Error", http.StatusInternalServerError)
+	} else {
+		log.Print("-> 500 ", message)
+		http.Error(response, logMessage, http.StatusInternalServerError)
+	}
 }
 
-func readBodyJson(request *Request, value interface{}) error {
-	decoder := json.NewDecoder(request.Body)
-	return decoder.Decode(value)
+func httpInvalidRequest(response Response, message string) {
+	logMessage := "Invalid request: " + message
+	log.Print("->", http.StatusBadRequest, logMessage)
+	http.Error(response, logMessage, http.StatusBadRequest)
+}
+
+func httpSuccess(response Response) {
+	log.Print("-> 200 OK")
+	http.Error(response, "", http.StatusOK)
 }
