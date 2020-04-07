1f422e7d6292f6823fe9005c92006f23b7977757
S. B. Immington <shadowroller@immington.industries>
2020-04-07 19:53:43 -0400

    Add JWT for auth

---
+++ auth.go
@@ -0,0 +1,63 @@
+package srserver
+
+// Authentication
+
+import (
+	"encoding/base64"
+	"github.com/dgrijalva/jwt-go"
+	jwtRequest "github.com/dgrijalva/jwt-go/request"
+	"log"
+	"math/rand"
+	"srserver/config"
+)
+
+type AuthClaims struct {
+	GameID   string `json:"gid"`
+	PlayerID string `json:"pid"`
+	jwt.StandardClaims
+}
+
+func GenUID() string {
+	bytes := make([]byte, 8)
+	rand.Read(bytes)
+	return base64.RawURLEncoding.EncodeToString(bytes)
+
+}
+
+func getJWTSecretKey(token *jwt.Token) (interface{}, error) {
+	if token.Method != jwt.SigningMethodHS256 {
+		return nil, jwt.ErrInvalidKeyType
+	}
+	return config.JWTSecretKey, nil
+}
+
+func createAuthToken(gameID string, playerID string) (string, error) {
+	claims := AuthClaims{
+		gameID,
+		playerID,
+		jwt.StandardClaims{
+			Issuer: "sr-server",
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signed, err := token.SignedString(config.JWTSecretKey)
+	return signed, err
+}
+
+func authForRequest(request *Request) (*AuthClaims, error) {
+	token, err := jwtRequest.ParseFromRequest(
+		request,
+		jwtRequest.AuthorizationHeaderExtractor,
+		getJWTSecretKey,
+		jwtRequest.WithClaims(AuthClaims{}),
+	)
+	if err != nil {
+		return nil, err
+	}
+	auth, ok := token.Claims.(*AuthClaims)
+	if !ok || !token.Valid {
+		return nil, jwt.ErrInvalidKeyType
+	}
+	log.Printf("Retrieved auth claims: %v", auth)
+	return auth, nil
+}
--- config/config.go
+++ config/config.go
@@ -10,6 +10,7 @@ var (
 	// Server configs
 	ServerAddress   = readString("SERVER_ADDRESS", ":3001")
 	FrontendAddress = readString("FRONTEND_ADDRESS", "localhost:3000")
+	JWTSecretKey    = []byte(readString("SECRET_JWT", "1337s3cr3t"))
 	// TLS configs
 	TlsEnable = readBool("TLS_ENABLE", false)
 	TlsHost   = readString("TLS_HOST", "https://shadowroller.immington.industries")
@@ -19,12 +20,14 @@ var (
 	WriteTimeoutSecs    = readInt("WRITE_TIMEOUT_SECS", 30)
 	IdleTimeoutSecs     = readInt("IDLE_TIMEOUT_SECS", 60)
 	SSEWriteTimeoutSecs = readInt("SSE_WRITE_TIMEOUT_SECS", 30)
+	SSERetryTimeSecs    = readInt("SSE_RETRY_TIME_SECS", 10)
 	MaxHeaderBytes      = readInt("MAX_HEADER_BYTES", 1<<20)
 	// LibraryOptions
 	RedisUrl = readString("REDIS_URL", "redis://redis:6379")
 	// Backend options
 	HardcodedGameNames = readString("GAME_NAMES", "testgame1,testgame2")
 	RollBufferSize     = readInt("ROLL_BUFFER_SIZE", 200)
+	MaxSingleRoll      = readInt("MAX_SINGLE_ROLL", 100)
 )
 
 func readString(name string, defaultValue string) string {
@@ -36,7 +39,11 @@ func readString(name string, defaultValue string) string {
 }
 
 func readInt(name string, defaultValue int) int {
-	val, err := strconv.Atoi(name)
+	envVal, ok := os.LookupEnv("SR_" + name)
+	if !ok {
+		return defaultValue
+	}
+	val, err := strconv.Atoi(envVal)
 	if err != nil {
 		return defaultValue
 	}
@@ -44,7 +51,11 @@ func readInt(name string, defaultValue int) int {
 }
 
 func readBool(name string, defaultValue bool) bool {
-	val, err := strconv.ParseBool(name)
+	envVal, ok := os.LookupEnv("SR_" + name)
+	if !ok {
+		return defaultValue
+	}
+	val, err := strconv.ParseBool(envVal)
 	if err != nil {
 		return defaultValue
 	}
