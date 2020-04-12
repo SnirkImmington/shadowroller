2cba7b35559d2554fcbae25802ffb5f116c7880d
S. B. Immington <snirk.immington@gmail.com>
2020-04-12 11:32:00 -0400

    Gonna have to do redis scan

--- event.go
+++ event.go
@@ -9,22 +9,22 @@ import (
 
 // EventCore contains the type of events
 type EventCore struct {
-	ID   string `json:"id,omitempty"`
-	Type string `json:"ty"`
+	//ID   string `json:"id,omitempty"` // specially written by Redis
+	Type string `json:"ty",redis:"ty"`
 }
 
 // RollEvent is posted when a player rolls dice.
 type RollEvent struct {
 	EventCore
-	PlayerID string `json:"pID"`
-	Roll     []int  `json:"roll"`
+	PlayerID string `json:"pID",redis:"pID"`
+	Roll     []int  `json:"roll",redis"roll"`
 }
 
 // PlayerJoinEvent is posted when a new player joins the game.
 type PlayerJoinEvent struct {
 	EventCore
-	PlayerID   string `json:"pid"`
-	PlayerName string `json:"pName"`
+	PlayerID   string `json:"pid",redis:"pid"`
+	PlayerName string `json:"pName",redis:"pName"`
 }
 
 // Event interface determines what is sent to postEvent
@@ -58,7 +58,7 @@ func receiveEvents(gameID string) (<-chan string, chan<- bool) {
 		for {
 			// See if we've been canceled.
 			select {
-			case _ = <-okChan:
+			case <-okChan:
 				log.Printf("%v: Canceling event read for %v", goID, gameID)
 				return
 			default:
@@ -66,7 +66,7 @@ func receiveEvents(gameID string) (<-chan string, chan<- bool) {
 			}
 			log.Print(goID, " Checking Redis")
 
-			resp, err := redis.Values(conn.Do(
+			data, err := redis.Values(conn.Do(
 				"XREAD", "BLOCK", 0, "STREAMS", "event:"+gameID, "$",
 			))
 			if err != nil {
@@ -74,19 +74,77 @@ func receiveEvents(gameID string) (<-chan string, chan<- bool) {
 				return
 			}
 
-			log.Print(goID, " Here's the base array: ")
-
-			log.Print("Scanning channel")
-			var channel []string
-			resp, err = redis.Scan(resp, &channel)
+			var channels [][]interface{}
+			err = redis.ScanSlice(data, &channels)
 			if err != nil {
-				log.Print(goID, " could not read channel:", err)
+				log.Print(goID, " unable to scan slice: ", err)
+				return
 			}
-			log.Printf("%v Well, got %#v", channel)
+			log.Print(goID, "Got ", len(channels), " response")
+			for i := 0; i < len(channels); i++ {
+				ch := channels[i]
+				var keyName string
+				var events []interface{}
+				rest, err := redis.Scan(ch, &keyName, &events)
+				if err != nil {
+					log.Print(goID, " Unable to scan key ", err)
+					return
+				}
+				log.Print(goID, " Got ", keyName, " -> ", events, ", ", rest)
 
-			log.Printf("%v %v received ", goID, resp)
+			}
+
+			log.Print("\n\n", goID, " Checking out some data:", data)
+			// We're only reading from one game's event list.
+			var channelResponse struct {
+				eName string
+				eval  []interface{}
+			}
+			_, err = redis.Scan(data, &channelResponse)
+			if err != nil {
+				log.Print(err)
+			} else {
+				log.Print(goID, " got ", channelResponse, " okay")
+			}
+			/*
+				log.Print("Found ", len(channels), " channels")
+
+				for i := 0; i < len(channels); i++ {
+					log.Printf("Scanning a channel")
+					var channelName string
+					_, err = redis.Scan(channels[i].([]interface{}), &channelName)
+					log.Printf("It's ", channelName)
+				}
+			*/
+
+			/*
+				var channel string
+				streamName := redis.Scan(mainStream, &cannel)
+
+				log.Print(goID, " Here's the base array: ", mainStream)
+				log.Print(goID, " with conversion: ", bytesToStrings(resp))
+			*/
 			eventsChan <- "got one"
 		}
 	}()
 	return eventsChan, okChan
+}
+
+/*
+   chans [
+     keyName string
+     responses [
+         id string
+         value HASHMAP
+     ]
+   ]
+*/
+
+type StreamResponse struct {
+	Channels []ChannelResponse
+}
+
+type ChannelResponse struct {
+	Key       string
+	responses []Event
 }
