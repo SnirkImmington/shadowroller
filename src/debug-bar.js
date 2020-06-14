f587a067f91b9bab5c5906a8b5cddad458bba90b
S. B. Immington <shadowroller@immington.industries>
2020-06-14 15:08:37 -0400

    Add auth cookie to debug bar

--- src/debug-bar.js
+++ src/debug-bar.js
@@ -5,40 +5,61 @@ import styled from 'styled-components/macro';
 import type { StyledComponent } from 'styled-components';
 import * as UI from 'style';
 
+import * as server from 'server';
 import * as Game from 'game';
 import * as Event from 'event';
 
-const StyledBar: StyledComponent<> = styled(UI.FlexRow)`
+const StyledBar: StyledComponent<> = styled(UI.FlexColumn)`
     padding: 4px 8px;
     color: white;
     background-color: ${({theme}) => theme.colors.primary}b0;
     font-size: 14px;
     flex-wrap: wrap;
+    text-wrap: wrap;
+    line-height: 1.25em;
 `;
 
 export default function DebugBar() {
     const gameState = React.useContext(Game.Ctx);
     const game = !gameState ? gameState :
-        {...gameState, players: undefined};
-    const players = !gameState ? [] : Object.fromEntries(gameState.players.entries());
+        {...gameState, player: undefined, players: undefined};
+    const players = !gameState ? "N/A" : gameState.players.size;
+
+    const cookie = document.cookie;
+    const auth = React.useMemo(() => {
+        try {
+            const authMatch = cookie.match(/srAuth=[^.]+.([^.]+)/);
+            return atob(authMatch[1]);
+        }
+        catch {
+            return "N/A";
+        }
+    }, [cookie]);
+
     return (
         <StyledBar>
-            <div>
-                <b>Shadowroller development</b>
-                &nbsp;
-                <UI.LinkButton light>button</UI.LinkButton>
-                &nbsp;
-                <UI.LinkButton light disabled>button</UI.LinkButton>
-            </div>
-
-            <div style={{marginLeft: 'auto'}}>
-                <b>Game state:&nbsp;</b>
-                <tt>{JSON.stringify(game) ?? 'undefined'}</tt>
-            </div>
-            <div style={{marginLeft: 'auto'}}>
-                <b>Players:&nbsp;</b>
-                <tt>{JSON.stringify(players)}</tt>
-            </div>
+            <UI.FlexRow wrap>
+                <div>
+                    <b>
+                        Shadowroller {process.env.NODE_ENV}&nbsp;
+                    </b>
+                    <tt>{server.BACKEND_URL}</tt>&nbsp;
+                </div>
+                <div style={{marginLeft: 'auto' }}>
+                    <b>Game:&nbsp;</b>
+                    <tt>{JSON.stringify(game) ?? 'undefined'}</tt>
+                </div>
+            </UI.FlexRow>
+            <UI.FlexRow wrap>
+                <div style={{flex: 'flex-shrink'}}>
+                    <b>Auth:</b>&nbsp;
+                    <tt>{auth}</tt>
+                </div>
+                <div style={{marginLeft: 'auto'}}>
+                    <b>Players:&nbsp;</b>
+                    <tt>{JSON.stringify(players)}</tt>
+                </div>
+            </UI.FlexRow>
         </StyledBar>
     );
 }
