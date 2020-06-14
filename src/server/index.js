8cf90da41bafc53ea7c95f98c9b8508e40918c32
S. B. Immington <shadowroller@immington.industries>
2020-06-14 15:09:37 -0400

    Derive non-prod server IP from document.location

--- src/server/index.js
+++ src/server/index.js
@@ -6,7 +6,8 @@ import * as Event from 'event';
 import * as events from './events';
 
 export const BACKEND_URL = process.env.NODE_ENV === 'production' ?
-    'https://shadowroller.immington.industries/' : 'http://localhost:3001/';
+    'https://shadowroller.immington.industries/'
+    : document.location.toString().replace(':3000', ':3001');
 
 export type Connection = "offline" | "connecting" | "connected" | "errored" | "disconnected";
 export type SetConnection = (Connection) => void;
