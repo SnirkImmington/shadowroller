359c31e0d0970d3f75d689aa00cf6f2d6a8d0b46
S. B. Immington <shadowroller@immington.industries>
2020-06-16 00:27:57 -0400

    Nice mobile formatting for debug bar

--- src/debug-bar.js
+++ src/debug-bar.js
@@ -7,7 +7,6 @@ import * as UI from 'style';
 
 import * as server from 'server';
 import * as Game from 'game';
-import * as Event from 'event';
 import { ConnectionCtx } from 'connection';
 
 const StyledBar: StyledComponent<> = styled(UI.FlexColumn)`
@@ -15,8 +14,30 @@ const StyledBar: StyledComponent<> = styled(UI.FlexColumn)`
     color: white;
     background-color: ${({theme}) => theme.colors.primary}b0;
     font-size: 14px;
-    flex-wrap: wrap;
     line-height: 1.25em;
+    width: 100%;
+
+    height: 6.25em;
+    overflow-y: scroll;
+    @media all and (min-width:768px) {
+        height: auto;
+        overflow: inherit;
+    }
+`;
+
+const Group = styled(UI.ColumnToRow).attrs(() => ({
+    maxWidth: true, flexWrap: true,
+}))`
+    @media all and (min-width: 768px) {
+        justify-content: space-between;
+    }
+`;
+
+const Item = styled.div`
+    width: 100%;
+    @media all and (min-width: 768px) {
+        width: auto;
+    }
 `;
 
 export default function DebugBar() {
@@ -39,32 +60,32 @@ export default function DebugBar() {
 
     return (
         <StyledBar>
-            <UI.FlexRow wrap>
-                <div style={{flexGrow: '1'}}>
+            <Group>
+                <Item>
                     <b>
                         Shadowroller {process.env.NODE_ENV}&nbsp;
                     </b>
                     <tt>{server.BACKEND_URL}</tt>&nbsp;
-                </div>
-                <div style={{flexGrow: 1}}>
+                </Item>
+                <Item>
                     <b>Connection:&nbsp;</b>
                     <tt>{connection}</tt>
-                </div>
-                <div style={{marginLeft: 'auto' }}>
-                    <b>Game:&nbsp;</b>
-                    <tt>{JSON.stringify(game) ?? 'undefined'}</tt>
-                </div>
-            </UI.FlexRow>
-            <UI.FlexRow wrap>
-                <div style={{flex: 'flex-shrink'}}>
+                </Item>
+                <Item>
                     <b>Auth:</b>&nbsp;
                     <tt>{auth}</tt>
-                </div>
-                <div style={{marginLeft: 'auto'}}>
+                </Item>
+            </Group>
+            <Group>
+                <Item>
+                    <b>Game:&nbsp;</b>
+                    <tt>{JSON.stringify(game) ?? 'undefined'}</tt>
+                </Item>
+                <Item>
                     <b>Players:&nbsp;</b>
                     <tt>{JSON.stringify(players)}</tt>
-                </div>
-            </UI.FlexRow>
+                </Item>
+            </Group>
         </StyledBar>
     );
 }
