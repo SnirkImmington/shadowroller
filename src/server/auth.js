fe551409741727b99f47989cf187f25465de3b9f
S. B. Immington <snirk.immington@gmail.com>
2020-07-19 12:07:42 -0400

    Don't use localforage

--- src/server/auth.js
+++ src/server/auth.js
@@ -2,11 +2,26 @@
 
 import * as Game from 'game';
 import * as connection from 'connection';
+import localforage from 'localforage';
 
-export function getLocalAuth(): ?string {
-    return localStorage.getItem("shadowroller.auth");
+export type ACCESS_STATE = "loading" | "found";
+export type SAVING_STATE = "saving" | "saved";
+
+export let credentials: ?string = null;
+export let session: ?string = null;
+
+export function getAuthCredentials(): ?string {
+    return localStorage.getItem("authCredentials");
+}
+
+export function saveAuthCredentials(credentials: string) {
+    localStorage.setItem("authCredentials", credentials);
 }
 
 export function saveSession(session: string) {
-    localStorage.setItem("shadowroller.session", session);
+    sessionStorage.setItem("session", session);
+}
+
+export function getSession(): ?string {
+    return sessionStorage.getItem("session");
 }
