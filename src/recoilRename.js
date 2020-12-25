5b8a178da93c7f41fd81ff8975d335e13dfb91ea
S. B. Immington <snirk.immington@gmail.com>
2020-12-25 15:38:05 -0500

    Add initial recoil code for streamprovider

---
+++ src/recoilRename.js
@@ -0,0 +1,8 @@
+import * as Recoil from 'recoil';
+
+export * from 'recoil';
+
+export const useState = Recoil.useSetRecoilState;
+export const useSetState = Recoil.useSetRecoilState;
+export const useGetState = Recoil.useRecoilValue;
+export const useResetState = Recoil.useResetRecoilState;
--- src/stream.js
+++ src/stream.js
@@ -1,16 +1,112 @@
 // @flow
 
 import * as React from 'react';
+import * as Recoil from 'recoilRename';
 
-import * as Event from 'event';
+import * as Events from 'event';
 import * as server from 'server';
-import type { SetConnection } from 'connection';
+import type { Connection, SetConnection } from 'connection';
 
-export type State = ?EventSource;
-export type Setter = (State) => void;
+type StreamConnection = Connection | "reconnecting";
 
-export const Ctx = React.createContext<State>(null);
-export const SetterCtx = React.createContext<Setter>(() => {});
+const streamState = Recoil.atom<?EventSource>({
+    key: "stream.stream",
+    default: null
+});
+
+const connectionState = Recoil.atom<StreamConnection>({
+    key: "stream.connection",
+    default: "offline"
+});
+
+type RetryState = { count: number, timer: ?TimeoutID }
+
+const retriesState = Recoil.atom<RetryState>({
+    key: "stream.retries",
+    default: { count: 0, timer: null }
+});
+
+const isConnected = Recoil.selector<bool>({
+    key: "stream.isConnected",
+    get: ({get}) => {
+        return get<Connection>(connectionState).ty === "connected";
+    }
+});
+
+function openStream(url, get, set) {
+    const source = new EventSource(url);
+    set(streamState, source);
+    set(connectionState, "connecting");
+    source.onopen = function() { streamOnOpen(source, get, set); };
+    source.onerror = function() { streamOnError(source, get, set); };
+    return source;
+}
+
+function streamOnOpen(stream: EventSource, get, set) {
+    // TODO game ID
+    const gameID = "TODO Game ID";
+    if (process.env.NODE_ENV !== "production") {
+        document.title = `${gameID} - Shadowroller (${process.env.NODE_ENV})`;
+    }
+    else {
+        document.title = `${gameID} - Shadowroller`;
+    }
+    const { timer } = get(retriesState);
+    clearTimeout(timer);
+    set(retriesState, { count: 0, timer: null }); // TOOD reset
+    set(connectionState, "connected");
+}
+
+const REOPEN_DELAYS = [
+    2, 2, 4, 4, 8, 8, 16, 16
+].map(s => s * 1000);
+
+const REOPEN_MAX = 32 * 1000;
+
+function streamOnError(source: EventSource, get, set) {
+    set(connectionState, "reconnecting");
+    set(streamState, null);
+    const { count } = get(retriesState);
+    const delay = count > REOPEN_DELAYS.length ? REOPEN_MAX : REOPEN_DELAYS[count];
+    const timer = setTimeout(
+        function retry() {
+            const url = source.url;
+            const stream = openStream(url, get, set);
+        },
+        delay
+    );
+    set(retriesState, s => { count: s.count, timer });
+
+}
+
+function streamOnEvent({get, set}) {
+
+}
+
+const connect = Recoil.selector<string>({
+    key: "stream.connect",
+    set: ({get, set}, value: string) => {
+        const connection = get<Connection>(connectionState);
+        if (connection !== "offline") {
+            return;
+        }
+        openStream(value, get, set);
+    }
+});
+
+const disconnect = Recoil.selector<void>({
+    key: "stream.disconnect",
+    set: ({get, set}, _) => {
+        set(connectionState, "offline");
+        set(retriesState, { count: 0, timer: null }); // TODO reset
+        const source = get<?EventSource>(streamState);
+        if (source) {
+            source.close();
+            set(streamState, null);
+        }
+    }
+});
+
 
 function onMessage(event: MessageEvent) {
     if (process.env.NODE_ENV !== "production" && event.data !== "") {
