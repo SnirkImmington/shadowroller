319841d2c1661bb357ea480ee3b535321396886f
S. B. Immington <shadowroller@immington.industries>
2020-12-29 18:33:03 -0500

    Update connection to use RetryConnection

--- src/connection.js
+++ src/connection.js
@@ -4,10 +4,11 @@ import * as React from 'react';
 
 export type Connection = "offline" | "connecting" | "connected" | "errored" | "disconnected";
 export type RetryConnection = Connection | "retrying";
-export type SetConnection = (RetryConnection | (RetryConnection => RetryConnection)) => void;
+export type SetConnection = (Connection | (Connection => Connection)) => void;
+export type SetRetryConnection = (RetryConnection | (RetryConnection => RetryConnection)) => void;
 
 export const ConnectionCtx = React.createContext<RetryConnection>("connected");
-export const SetConnectionCtx = React.createContext<SetConnection>(() => {});
+export const SetConnectionCtx = React.createContext<SetRetryConnection>(() => {});
 
 export function connectionFor(response: Response): Connection {
     if (!response.status) {
