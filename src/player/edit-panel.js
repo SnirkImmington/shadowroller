8137605c48915435c2d6550c7b9f1f823701a369
S. B. Immington <shadowroller@immington.industries>
2021-01-01 11:52:57 -0500

    Update EditPanel to use player context

--- src/player/edit-panel.js
+++ src/player/edit-panel.js
@@ -9,9 +9,10 @@ import theme from 'style/theme';
 import * as Game from 'game';
 import * as Event from 'history/event';
 import * as server from 'server';
-import * as Stream from '../stream';
+import * as Player from 'player';
+import * as Stream from 'stream-provider';
 import * as srutil from 'srutil';
-import { SetConnectionCtx } from 'connection';
+import { StatusText, ConnectionCtx, SetConnectionCtx } from 'connection';
 
 import ColorPicker from 'color-picker';
 import { EventRecord } from 'history/history-panel';
@@ -29,30 +30,27 @@ type Props = {
 
 export default function EditPlayerPanel({ hide }: Props) {
     const game = React.useContext(Game.Ctx);
+    const player = React.useContext(Player.Ctx);
     const gameDispatch = React.useContext(Game.DispatchCtx);
     const eventDispatch = React.useContext(Event.DispatchCtx);
-    const stream = React.useContext(Stream.Ctx);
-    const setStream = React.useContext(Stream.SetterCtx);
+    const playerDispatch = React.useContext(Player.DispatchCtx);
+    const [_, logout] = React.useContext(Stream.Ctx);
+    const connection = React.useContext(ConnectionCtx);
     const setConnection = React.useContext(SetConnectionCtx);
 
-    const [name, setName] = React.useState(game?.player?.name);
-    const [hue, setHue] = React.useState(0);
+    const [name, setName] = React.useState(player?.name);
+    const [hue, setHue] = React.useState(player?.hue || 0);
     const [loading, setLoading] = React.useState(false);
 
     const [exampleTitle, updateTitleFlavor] = srutil.useFlavor(ROLL_TITLE_FLAVOR);
     const [dice] = React.useState(() => srutil.roll(11));
-    const [initiativeDice] = React.useState(() => srutil.roll(2));
 
-    if (!game) {
+    if (!player || !game) {
         return null;
     }
 
-    const changed = name !== game.player.name || hue != 0;
-
-    let displayName = name || game.player.name;
-
-
-    let title = `Connected to ${game.gameID} as ${game.player.name}`
+    const changed = name !== player.name || hue != 0;
+    const connected = connection === "connected";
 
     function onSubmit(e) {
         e.preventDefault();
@@ -62,32 +60,26 @@ export default function EditPlayerPanel({ hide }: Props) {
 
     function onLogout(e) {
         e.preventDefault();
-        server.handleLogout(stream, setStream, setConnection, gameDispatch, eventDispatch);
+        logout();
     }
 
-    const exampleInitiative: Event.Initiative = {
-        ty: "initiativeRoll", id: new Date().valueOf(),
-        source: { id: game.player.id, name: displayName },
-        title: "", base: 13, dice: initiativeDice
-    }
-
+    let displayName = name || player.name;
     const exampleEvent: Event.Roll = {
         ty: "roll", id: new Date().valueOf(),
-        source: { id: game.player.id, name: displayName },
+        source: { id: player.id, name: displayName },
         title: exampleTitle, dice, glitchy: 0
     }
 
     const hueColor = `hsl(${hue}, 80%, 56%)`;
 
-
     return (
         <UI.Card color={theme.colors.primary}>
             <UI.FlexRow maxWidth floatRight>
                 <UI.CardTitleText color={theme.colors.primary}>
-                    Options for {game.player.name}
+                    Options for {player.name}
                 </UI.CardTitleText>
                 <UI.FlexRow spaced>
-                    <UI.LinkButton>log out</UI.LinkButton>
+                    <UI.LinkButton onClick={onLogout}>log out</UI.LinkButton>
                     <span />
                     <UI.LinkButton minor onClick={hide}>close</UI.LinkButton>
                 </UI.FlexRow>
@@ -96,13 +88,14 @@ export default function EditPlayerPanel({ hide }: Props) {
                 <UI.FlexColumn>
                     <UI.FlexRow formRow justifyContent="space-around">
                         <EventRecord style={{}} noActions setHeight={() => {}}
-                            playerID={game.player.id} color={hueColor} event={exampleEvent} />
+                            playerID={player.id} color={hueColor} event={exampleEvent} />
                     </UI.FlexRow>
                     <UI.ColumnToRow>
                         <span style={{ flexGrow: 1}} />
                         <UI.FlexRow formRow>
                             Player name
-                            <UI.Input value={name} onChange={e => setName(e.target.value)} />
+                            <UI.Input value={name} placeholder={player.name}
+                                      onChange={e => setName(e.target.value)} />
                         </UI.FlexRow>
                         <UI.FlexRow formRow>
                             <label htmlFor="player-settings-hue">Hue</label>
@@ -112,8 +105,9 @@ export default function EditPlayerPanel({ hide }: Props) {
                         <span style={{ flexGrow: 1 }} />
                     </UI.ColumnToRow>
                     <UI.FlexRow spaced>
+                        <StatusText connection={connection}/>
                         <span style={{flexGrow: 1}} />
-                        <UI.LinkButton disabled={!changed}>update</UI.LinkButton>
+                        <UI.LinkButton disabled={!changed || !connected}>update</UI.LinkButton>
                         <UI.LinkButton minor onClick={hide}>cancel</UI.LinkButton>
                     </UI.FlexRow>
                 </UI.FlexColumn>
