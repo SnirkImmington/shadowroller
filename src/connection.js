bceb0e519e6b31fd6416a2a3fcbe1a51c6c4db1c
S. B. Immington <shadowroller@immington.industries>
2021-01-01 11:43:40 -0500

    Add StatusText for connection

--- src/connection.js
+++ src/connection.js
@@ -1,6 +1,7 @@
 // @flow
 
 import * as React from 'react';
+import * as UI from 'style';
 
 export type Connection = "offline" | "connecting" | "connected" | "errored" | "disconnected";
 export type RetryConnection = Connection | "retrying";
@@ -36,4 +37,25 @@ export function statusFor(response: Response): ResponseStatus {
     else {
         return "badRequest";
     }
+}
+
+export function StatusText({ connection }: { connection: RetryConnection }) {
+    let text = null;
+    switch (connection) {
+        case "connecting":
+        case "disconnected":
+        case "errored":
+            text = connection;
+        case "retrying":
+            text = "Reconnecting";
+    }
+    if (text) {
+        return (
+            <i>
+                <UI.DiceSpinner />
+                {text}...
+            </i>
+        );
+    }
+    return null;
 }
