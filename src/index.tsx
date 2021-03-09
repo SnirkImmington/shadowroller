fe448f7c159bd4bb93a14e48baaf54a454b78a5e
S. B. Immington <shadowroller@immington.industries>
2021-03-08 20:06:34 -0500

    Add presite to pre-render the site

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,16 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const root = document.getElementById("root");
 
 if (process.env.NODE_ENV !== 'production') {
     document.title = `Shadowroller (${process.env.NODE_ENV})`;
+    ReactDOM.render(
+        <React.StrictMode><App /></React.StrictMode>,
+        root
+    );
 }
-
-ReactDOM.render(
-    (
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>
-    ),
-    document.getElementById('root')
-);
-serviceWorker.unregister();
+else {
+    ReactDOM.hydrate(<App />, root);
+}
