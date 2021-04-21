e0fcb324fefe8a5f8e1b944332f97e26ee002c24
S. B. Immington <shadowroller@immington.industries>
2021-04-20 22:20:10 -0400

    Add comment docs for rollStats

--- web/src/rollStats.tsx
+++ web/src/rollStats.tsx
@@ -1,13 +1,20 @@
 import * as Event from 'event';
 
+/** sumOnes counts the number of 1s in a roll. */
 function sumOnes(dice: number[]): number {
     return dice.reduce((acc, curr) => curr === 1 ? acc + 1 : acc, 0);
 }
 
+/** sumHits counts the number of hits (5 or 6) in a roll. */
 function sumHits(dice: number[]): number {
     return dice.reduce((acc, curr) => curr >= 5 ? acc + 1 : acc, 0);
 }
 
+/** isGlitched determines if a roll is glitched.
+    - If a glitch was rerolled, it is still a glitch.
+    - A roll can be rerolled into a [critical] glitch.
+    - A limit-pushed roll can also be a glitch.
+ */
 export function isGlitched(event: Event.DiceEvent): boolean {
     if (event.ty === "roll") {
         // Glitches are phrased (p. 45) "more than half the dice you rolled show a one"
@@ -39,17 +46,34 @@ export function isGlitched(event: Event.DiceEvent): boolean {
     }
 }
 
+/** HitsResults are statistics and facts about a roll. */
 export type HitsResults = {
+    /** Total result pool. */
     dice: number[],
+    /** Total number of hits achieved. */
     hits: number,
+    /** Glitchiness modifier (+- ones to glitch) requested */
     glitchy: number,
+    /** Whether the roll glitched (see `isGliched`) */
     glitched: boolean,
+    /** Whether the roll critically glitched */
     critical: boolean,
+    /** Whether the roll is worthy of being displayed
+       This currently applies for glitches, > 12 dice, or rolls with >4 hits.
+     */
     shouldDisplay: boolean,
+    /** Whether edge was spent on the roll */
     edged: boolean,
+    /** Whether the roll was rerolled */
     rerolled: boolean,
+    /** The number of times the dice were rolled:
+        - 1 for regular rolls
+        - 2 for reroll failures
+        - 2+ for push the limit
+     */
     rounds: number,
 };
+/** results provides `HitsResults` for the given [edged] roll. */
 export function results(event: Event.DiceEvent): HitsResults {
     const dice: number[] = "dice" in event ? event.dice : event.rounds.flat();
     const rerolled = event.ty === "rerollFailures";
@@ -79,6 +103,7 @@ export function results(event: Event.DiceEvent): HitsResults {
     }
 }
 
+/** resultMessage provides text to display in a roll record. */
 export function resultMessage(results: HitsResults): string {
     const hits = results.hits === 1 ? "hit" : "hits";
     if (results.critical) {
