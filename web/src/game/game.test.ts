7ab3ea5636cbef6565af1bed275afe006aa5c8ee
S. B. Immington <shadowroller@immington.industries>
2021-04-24 20:51:18 -0400

    Add basic game dispatch() tests

--- web/src/game/game.test.ts
+++ web/src/game/game.test.ts
@@ -27,13 +27,6 @@ export function mockState(): Game.Game {
     };
 }
 
-// Things to test
-// - join
-// - leave
-// - playerUpdate
-// - setPlayers
-// - default
-
 describe("reduce()", function() {
     it("handles joining a new game", function() {
         const action: Game.Action = {
@@ -49,12 +42,63 @@ describe("reduce()", function() {
         const action: Game.Action = {
             ty: "join", gameID: "bar", players: new Map()
         };
+        const state = mockState();
+        const result = Game.reduce(state, action);
+        expect(result).not.toBeNull();
+        expect(result.gameID).toBe("bar");
+    });
+
+    it("handles leaving an existing game", function() {
+        const action: Game.Action = {
+            ty: "leave",
+        };
         const state: Game.State = {
             gameID: "foo",
             players: new Map()
         };
         const result = Game.reduce(state, action);
-        expect(result).not.toBeNull();
-        expect(result.gameID).toBe("bar");
+        expect(result).toBeNull();
     });
+
+    it("handles a player update", function() {
+        const action: Game.Action = {
+            ty: "playerUpdate",
+            id: "player1ID",
+            update: {
+                name: "not player 1 name",
+                hue: 3
+            }
+        };
+        const state = mockState();
+        const result = Game.reduce(state, action);
+        expect(result).not.toBeNull();
+        const player1 = result.players.get("player1ID");
+        expect(player1).not.toBeNull();
+        expect(player1.name).toBe("not player 1 name");
+        expect(player1.hue).toBe(3);
+        expect(player1.online).toBe(false);
+    });
+
+    it("creates new player for a player update", function() {
+        const action: Game.Action = {
+            ty: "playerUpdate",
+            id: "newPlayerID",
+            update: {
+                name: "New player",
+                hue: 22,
+                online: true
+            }
+        };
+        const state = mockState();
+        const result = Game.reduce(state, action);
+        const newPlayer = result.players.get("newPlayerID");
+        expect(newPlayer).toEqual(action.update);
+    });
+
+    it("ignores an invalid action", function() {
+        const action = "This definitely isn't an action.";
+        const state = mockState();
+        const result = Game.reduce(state, action);
+        expect(result).toBe(state);
+    })
 });
