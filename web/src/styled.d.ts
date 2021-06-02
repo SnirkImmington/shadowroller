53833fa285bdfd3a734574a14c1552b902db7e52
S. B. Immington <shadowroller@immington.industries>
2021-06-02 18:10:03 -0400

    Update theme typing

--- web/src/styled.d.ts
+++ web/src/styled.d.ts
@@ -1,28 +1,6 @@
 import 'styled-components';
-import theme from 'style/theme';
+import type { Theme } from 'theme';
 
 declare module 'styled-components' {
-    export interface DefaultTheme {
-        colors: {
-            primary: string,
-            primaryLight: string,
-            primaryDesaturated: string,
-            primaryDesaturated3: string,
-
-            gray1: string,
-
-            secondary: string,
-            secondaryDark: string,
-            dieOne: string,
-            dieHit: string,
-            dieNone: string,
-
-            warning: string,
-
-            header: string
-        },
-        fonts: {
-            monospace: string
-        }
-    };
+    export interface DefaultTheme extends Theme { };
 }
