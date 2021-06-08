b7858bf695fc094a66b2d07c700aaed190998001
S. B. Immington <shadowroller@immington.industries>
2021-06-07 23:58:09 -0400

    Begin refining new code based on theme/layout

--- web/src/component/Button.tsx
+++ web/src/component/Button.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import styled, { CSSObject } from 'styled-components/macro';
-//import * as styledSystem from 'styled-system';
+
+import * as layout from 'layout';
+
 import type { IconDefinition } from '@fortawesome/fontawesome-common-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -10,50 +12,49 @@ const hasPointer: CSSObject = {
     cursor: "pointer"
 }
 
-const StyledButton = styled.button(props => ({
+type StyledButtonProps = {
+    /** If the button should be displayed with a more passive color */
+    minor?: boolean,
+}
+
+const StyledButton = styled.button<StyledButtonProps>(({ theme, minor }) => ({
     display: "inline",
 
     fontWeight: "bold",
-    fontSize: props.theme.fontSizes.reg,
+    fontSize: layout.FontSize.Regular,
     lineHeight: 1,
     ...hasPointer,
-    color: props.theme.colors.primary,
+    color: minor ? theme.colors.secondary : theme.colors.light,
     backgroundColor: "transparent",
 
     border: 0,
     outline: 0,
     padding: "2px",
-    borderBottom: `2px solid ${props.theme.colors.primary}`,
+    borderBottom: minor ? 0 : `2px solid ${theme.colors.light}`,
     whiteSpace: "pre",
 
+    ":enabled:hover": {
+        filter: "brightness(125%)",
+    },
     ":enabled:active": {
-        filter: "brightness(85%)"
+        filter: "brightness(80%)"
     },
     ":focus": {
         filter: "brightness(85%)"
     },
 
     ":disabled": {
-        cursor: "not-allowed !important",
-        borderBottom: "2px solid transparent",
-        color: props.theme.colors.neutral,
-    },
-    ":enabled:hover": {
-        filter: "brightness(125%)",
-    },
-
-    "& > svg:first-child": {
-        marginRight: props.theme.space.tiny,
-        height: "0.8em",
-        width: "0.8em"
+        cursor: "not-allowed",
+        borderBottom: minor ? 0 : "2px solid transparent",
+        color: theme.colors.neutral,
     },
 }));
 
-export const ButtonIcon = styled(FontAwesomeIcon)(props => ({
-    marginRight: props.theme.space.small,
-    height: "0.8em",
-    width: "0.8em",
-}));
+export const ButtonIcon = styled(FontAwesomeIcon)({
+    marginRight: layout.Space.Small,
+    height: "0.875em",
+    width: "0.875em",
+});
 
 export const MinorButton = styled(StyledButton)(props => ({
     color: props.theme.colors.secondary
@@ -61,9 +62,8 @@ export const MinorButton = styled(StyledButton)(props => ({
 
 export type ButtonProps = {
     icon?: IconDefinition,
-    children?: React.ReactChildren,
 };
-export function Button(props: ButtonProps) {
+export function Button(props: React.PropsWithChildren<ButtonProps>) {
     if (props.icon) {
         return (
             <StyledButton>
