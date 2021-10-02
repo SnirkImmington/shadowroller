f6243b6517cf67bed73fb12acf23dc384b6bc968
S. B. Immington <shadowroller@immington.industries>
2021-10-02 10:54:41 -0400

    Add more event tests

--- server/game/eventManager_test.go
+++ server/game/eventManager_test.go
@@ -4,11 +4,8 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"sync"
 	"testing"
 	"time"
-
-	//"time"
 
 	eventGen "sr/gen/event"
 	gameGen "sr/gen/game"
@@ -43,21 +40,139 @@ func TestPostEvent(t *testing.T) {
 		udBytes, err := json.Marshal(ud)
 		test.AssertSuccess(t, err, "writing update to json")
 
-		var wait sync.WaitGroup
-		wait.Add(1)
-		go func() {
-			defer wait.Done()
-			select {
-			case msg := <-sub.Messages():
-				test.AssertEqual(t, string(udBytes), msg.Message)
-			case <-time.After(time.Duration(5) * time.Second):
-				t.Error("Did not read update")
-			}
-		}()
+		wait := test.WaitForMessage(t, sub.Messages(), string(udBytes))
 
 		err = game.PostEvent(ctx, client, gameID, evt)
 		test.AssertSuccess(t, err, "event posted")
 
 		wait.Wait()
 	})
+
+	t.Run("event was added", func(t *testing.T) {
+		time.Sleep(time.Duration(50) * time.Millisecond)
+		evt := eventGen.Event(rng, plr)
+		evtStr, err := json.Marshal(evt)
+		test.AssertSuccess(t, err, "event was marshalled")
+
+		err = game.PostEvent(ctx, client, gameID, evt)
+		test.AssertSuccess(t, err, "event was posted")
+
+		found, err := event.GetByID(ctx, client, gameID, evt.GetID())
+		test.AssertSuccess(t, err, "event was found")
+		test.AssertEqual(t, string(evtStr), found)
+	})
+}
+
+func TestDeleteEvent(t *testing.T) {
+	ctx := context.Background()
+	rng := test.RNG()
+	db, client := test.GetRedis(t)
+
+	gameID := gameGen.GameID(rng)
+	err := game.Create(ctx, client, gameID)
+	test.AssertSuccess(t, err, "game created")
+	plr := playerGen.Player(rng)
+
+	t.Run("private update posted", func(t *testing.T) {
+		evt := eventGen.Event(rng, plr)
+		evt.SetShare(event.SharePrivate)
+		err = game.PostEvent(ctx, client, gameID, evt)
+		test.AssertSuccess(t, err, "event posted")
+
+		sub := db.NewSubscriber()
+		defer sub.Close()
+		privateChannel := fmt.Sprintf("update:%v:%v", gameID, plr.ID)
+		sub.Subscribe(privateChannel)
+		defer sub.Unsubscribe(privateChannel)
+
+		ud := update.ForEventDelete(evt.GetID())
+		udBytes, err := ud.MarshalJSON()
+		test.AssertSuccess(t, err, "update marshalled")
+
+		wait := test.WaitForMessage(t, sub.Messages(), string(udBytes))
+
+		err = game.DeleteEvent(ctx, client, gameID, evt)
+		test.AssertSuccess(t, err, "event deleted")
+
+		wait.Wait()
+	})
+
+	t.Run("event was deleted", func(t *testing.T) {
+		// Offset event ID
+		time.Sleep(time.Duration(50) * time.Millisecond)
+		evt := eventGen.Event(rng, plr)
+		evtStr, err := json.Marshal(evt)
+		test.AssertSuccess(t, err, "event was marshalled")
+
+		err = game.PostEvent(ctx, client, gameID, evt)
+		test.AssertSuccess(t, err, "event was posted")
+
+		found, err := event.GetByID(ctx, client, gameID, evt.GetID())
+		test.AssertSuccess(t, err, "event was found")
+		test.AssertEqual(t, string(evtStr), found)
+	})
+}
+
+func TestUpdateEventShare(t *testing.T) {
+	ctx := context.Background()
+	rng := test.RNG()
+	db, client := test.GetRedis(t)
+
+	gameID := gameGen.GameID(rng)
+	err := game.Create(ctx, client, gameID)
+	test.AssertSuccess(t, err, "game created")
+	plr := playerGen.Player(rng)
+
+	t.Run("private2game: single diff update posted", func(t *testing.T) {
+		evt := eventGen.Event(rng, plr)
+		evt.SetShare(event.SharePrivate)
+		err = game.PostEvent(ctx, client, gameID, evt)
+		test.AssertSuccess(t, err, "event posted")
+
+		sub := db.NewSubscriber()
+		defer sub.Close()
+		privateChannel := fmt.Sprintf("update:%v:%v", gameID, plr.ID)
+		sub.Subscribe(privateChannel)
+		defer sub.Unsubscribe(privateChannel)
+
+		ud := update.ForEventDiff(evt, map[string]interface{}{"share": event.ShareInGame})
+		udBytes, err := ud.MarshalJSON()
+		test.AssertSuccess(t, err, "update marshalled")
+
+		wait := test.WaitForMessage(t, sub.Messages(), string(udBytes))
+
+		err = game.UpdateEventShare(ctx, client, gameID, evt, event.ShareInGame)
+		test.AssertSuccess(t, err, "event share updated")
+
+		wait.Wait()
+	})
+
+	t.Run("event share was updated", func(t *testing.T) {
+		// Offset event ID
+		time.Sleep(time.Duration(50) * time.Millisecond)
+		evt := eventGen.Event(rng, plr)
+		err = game.PostEvent(ctx, client, gameID, evt)
+		test.AssertSuccess(t, err, "event was posted")
+
+		oldShare := evt.GetShare()
+		var newShare event.Share
+		for {
+			newShare = eventGen.Share(rng)
+			if newShare != oldShare {
+				break
+			}
+		}
+		t.Logf("Event %v share %v -> %v", evt.GetID(), oldShare, newShare)
+		err := game.UpdateEventShare(ctx, client, gameID, evt, newShare)
+		// evt's share has also been updated
+		test.AssertSuccess(t, err, "event share updated")
+
+		evtStr, err := json.Marshal(evt) // has new share
+		test.AssertSuccess(t, err, "event was marshalled")
+
+		found, err := event.GetByID(ctx, client, gameID, evt.GetID())
+		test.AssertSuccess(t, err, "event was found")
+		test.AssertEqual(t, string(evtStr), found)
+	})
+
 }
