1e02ce63ba8fe30b8a62f9524eb7a5d69b0330f1
S. B. Immington <shadowroller@immington.industries>
2022-01-22 10:13:57 -0500

    More work on resq

--- libsr/resq/listener.go
+++ libsr/resq/listener.go
@@ -4,25 +4,170 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"time"
 
 	"shadowroller.net/libsr/errs"
+	srOtel "shadowroller.net/libsr/otel"
+	"shadowroller.net/libsr/shutdown"
 
-	"github.com/go-redis/redis"
+	"go.opentelemetry.io/otel/trace"
+
+	"github.com/go-redis/redis/v8"
 )
+
+// Handler is the interface of functions to handle stream messages.
+type Handler interface {
+	HandleMessage(args *Args)
+}
+
+// HandlerFunc is a function which can handle a stream message.
+type HandlerFunc func(args *Args)
+
+func (h HandlerFunc) HandleMessage(args *Args) {
+	h(args)
+}
+
+// Args are arguments passed to a Handler.
+type Args struct {
+	Ctx     context.Context
+	Message *Message
+	Client  *redis.Client
+}
+
+// Get retrieves the fields of the Args
+func (args *Args) Get() (context.Context, *Message, *redis.Client) {
+	return args.Ctx, args.Message, args.Client
+}
 
 // Listener is the equivalent of http.Server which listens on a Redis client
 // for messages being posted to the queue.
 type Listener struct {
-	group    string
-	consumer string
-	client   *redis.Client
+	group    string        // stream group
+	consumer string        // stream consumer
+	client   *redis.Client // redis client
 	handlers map[string]Handler
 	streams  []string
+	lastID   map[string]string // last ID seen for each stream
+}
+
+// NewListener creates a new listener using the given client for the given group
+// and consumer. Each service should have one or more consumer groups, and should
+// have at least two "standard" listeners to account for basic concurrency.
+func NewListener(client *redis.Client, group string, consumer string) *Listener {
+	return &Listener{}
+}
+
+// HandleFunc registers the given HandlerFunc for the listener.
+func (l *Listener) HandleFunc(stream string, handler func(args *Args)) {
+	l.Handle(stream, HandlerFunc(handler))
+}
+
+// Handle registers the given Handler for the listener.
+func (l *Listener) Handle(stream string, handler Handler) {
+	if _, found := l.handlers[stream]; found {
+		panic(fmt.Sprintf("Attempted to double-register handler for %v", stream))
+	}
+	l.handlers[stream] = handler
+	l.streams = append(l.streams, stream)
+	l.lastID[stream] = "0-0" // Not sure if we need to track this given xclaim
+}
+
+func (l *Listener) runHandler(ctx context.Context, handler Handler, path string, id string, rawMessage map[string]string) {
+	var message *Message
+	//err := redisUtil.Parse(rawMessage, &request)
+	// if err !+ nil {
+	// // We need a way to forward this parse error here.
+	// }
+
+	// Get the span info from the request
+	var link trace.Link
+	spanContext, err := message.ParseSpanContext()
+	if err == nil {
+		link = trace.Link{
+			SpanContext: *spanContext,
+			// Attributes
+		}
+	} else if !errors.Is(err, errs.ErrNotFound) {
+		// This is the parse error
+	}
+	// Start a child span for the process
+	opts := []trace.SpanStartOption{
+		trace.WithSpanKind(trace.SpanKindConsumer),
+	}
+	if err == nil { // if link was set
+		opts = append(opts, trace.WithLinks(link))
+	}
+	name := fmt.Sprintf("handle %v %v", path, id) // Not sure what to put here
+	ctx, span := srOtel.Tracer.Start(ctx, name, opts...)
+	defer span.End()
+
+	if !message.Expires.IsZero() {
+		span.AddEvent("Apply timeout")
+		if time.Now().After(message.Expires) {
+			span.AddEvent("Message is expired")
+			return
+		}
+		ctx, _ = context.WithDeadline(ctx, message.Expires)
+		// Maybe do an initial check of the timeout here
+	}
+	if !message.After.IsZero() {
+		_, subSpan := srOtel.Tracer.Start(ctx, "sleep till request After")
+		time.Sleep(time.Until(message.After))
+		subSpan.End()
+	}
+
+	// We don't hold up the shutdown context in the handler, the handler can check
+	// the shutdown status itself if it wants to.
+	_, release := shutdown.Register(ctx, name)
+	defer release()
+
+	// handle defer
+	defer func() {
+		var data interface{}
+		if data = recover(); data == nil {
+			return
+		}
+
+		var err error
+		if passedErr, ok := data.(error); ok {
+			err = passedErr
+		} else {
+			err = fmt.Errorf("%w: unexpected panic data %v", errs.ErrLogic, data)
+		}
+
+		if errors.Is(err, errs.ErrHalt) {
+			return
+		}
+
+		if !errors.Is(err, errs.ErrHalt) {
+			span.RecordError(err)
+			srOtel.WithSetError(span, err, trace.WithStackTrace(true))
+		}
+		// If the error is a redis error, we may not be able to reply and stuff
+	}()
+
+	handler.HandleMessage(&Args{ctx, message, l.client})
+}
+
+func (l *Listener) handlePending(ctx context.Context) error {
+	// May want to loop this entire section.
+	results, err := l.client.Pipelined(ctx, func(pipe redis.Pipeliner) error {
+		for _, stream := range l.streams {
+			args := redis.XAutoClaimArgs{
+				Stream:   stream,
+				Group:    l.group,
+				Consumer: l.consumer,
+				Start:    "0-0",
+				// Not sure what this should be if we're trying to continue processing
+				MinIdle: time.Duration(4) * time.Second,
+			}
+		}
+	})
 }
 
 func (l *Listener) ListenAndServe(ctx context.Context) error {
 	// Check the pending queue for messages we missed
-	if err := l.HandlePending(ctx); err != nil {
+	if err := l.handlePending(ctx); err != nil {
 		return fmt.Errorf("handling pending: %w", err)
 	}
 
@@ -36,10 +181,11 @@ func (l *Listener) ListenAndServe(ctx context.Context) error {
 			Consumer: l.consumer,
 			Streams:  l.streams,
 			Count:    1,
+			Block:    time.Duration(4) * time.Second,
 			NoAck:    true,
 		}
-		req, err := l.client.XReadGroup(ctx, readArgs)
-		if errors.Is(err, context.ErrCanceled) {
+		entries, err := l.client.XReadGroup(ctx, readArgs).Result()
+		if errors.Is(err, context.Canceled) {
 			return errs.ErrHalt
 		} else {
 			// Retry? span?
---
+++ libsr/resq/message.go
@@ -0,0 +1,168 @@
+package resq
+
+import (
+	"context"
+	"encoding/json"
+	"time"
+
+	"shadowroller.net/libsr"
+	"shadowroller.net/libsr/errs"
+	srRedis "shadowroller.net/libsr/redis"
+
+	"go.opentelemetry.io/otel/trace"
+
+	"github.com/go-redis/redis/v8"
+)
+
+// Headers is metadata sent with each Message.
+type Headers struct {
+	TraceInfo string    `redis:"trace"`
+	From      string    `redis:"from"`
+	Expires   time.Time `redis:"exp,omitempty"`
+	After     time.Time `redis:"after,omitempty"`
+}
+
+// Message is a message sent over the queue.
+type Message struct {
+	Headers
+	Body []byte `redis:"body,omitempty"`
+}
+
+// Parse calls json.Unmarshal with r's Body
+func (r *Message) Parse(val interface{}) error {
+	return json.Unmarshal([]byte(r.Body), val)
+}
+
+// ParseTraceContext attempts to parse the Message's TraceContext.
+func (r *Message) ParseSpanContext() (*trace.SpanContext, error) {
+	if r.TraceInfo == "" {
+		return nil, errs.NotFoundf("No trace context saved")
+	}
+	var spanContext trace.SpanContext
+	err := json.Unmarshal([]byte(r.TraceInfo), &spanContext)
+	if err != nil {
+		return nil, errs.Internal(err)
+	}
+	return &spanContext, nil
+}
+
+func NewMessage(ctx context.Context, val *interface{}) (*Message, error) {
+	body, err := json.Marshal(val)
+	if err != nil {
+		return nil, errs.BadRequest(err)
+	}
+	spanCtx := trace.SpanContextFromContext(ctx)
+	ctxBody, err := json.Marshal(&spanCtx)
+	if err != nil {
+		return nil, errs.Internalf("unable to marshal span context: %w", err)
+	}
+	headers := Headers{
+		TraceInfo: string(ctxBody),
+		From:      libsr.ServiceName,
+		Expires:   time.Time{},
+		After:     time.Time{},
+	}
+	return &Message{
+		Headers: headers,
+		Body:    body,
+	}, nil
+}
+
+func SendMessage(ctx context.Context, client redis.Cmdable, stream string, msg *Message) (string, error) {
+	msgMap, err := srRedis.StructToStringMap(msg)
+	if err != nil {
+		return "", errs.BadRequest(err)
+	}
+	args := redis.XAddArgs{
+		Stream:     stream,
+		NoMkStream: false,
+		MaxLen:     1024,
+		Approx:     true,
+		Values:     msgMap,
+	}
+	return client.XAdd(ctx, &args).Result()
+}
+
+func SendReply(ctx context.Context, client redis.Cmdable, id string, reply *Reply) error {
+	bytes, err := json.Marshal(reply)
+	if err != nil {
+		return errs.BadRequest(err)
+	}
+	return client.LPush(ctx, "reply:"+id, bytes).Err()
+}
+
+func ReceiveReply(ctx context.Context, client redis.Cmdable, id string) (*Reply, error) {
+	// blpop returns key, value
+	popped, err := client.BLPop(ctx, time.Duration(4)*time.Second, id).Result()
+	if len(popped) != 2 {
+		return nil, errs.Internalf("expected 2 results from redis, got %v", popped)
+	}
+	var reply *Reply
+	err = json.Unmarshal([]byte(popped[1]), reply)
+	if err != nil {
+		return nil, err
+	}
+	return reply, nil
+}
+
+func ReceiveFirstReply(ctx context.Context, client redis.Cmdable, id string) (string, *Reply, error) {
+	popped, err := client.BLPop(ctx, time.Duration(4)*time.Second, id).Result()
+	if len(popped) != 2 {
+		return "", nil, errs.Internalf("expected 2 results from redis, got %v", popped)
+	}
+}
+
+func removeValueStrings(strings []string, value string) []string {
+	if len(strings) == 0 {
+		return strings
+	}
+	if len(strings) == 1 {
+		if value == strings[0] {
+			return []string{}
+		} else {
+			return strings
+		}
+	}
+	for ix, val := range strings {
+		if val != value {
+			continue
+		}
+		if ix == 0 {
+			return strings[1:]
+		}
+		if ix == len(strings)-1 {
+			return strings[:ix]
+		}
+		return append(strings[0:ix-1], strings[ix:]...)
+	}
+	return strings
+}
+
+// RecieveAllReplies listens for replies on all given IDs, and returns oll of the replies.
+func RecieveAllReplies(ctx context.Context, client redis.Cmdable, ids []string) (map[string]*Reply, error) {
+	dur := time.Duration(4) * time.Second
+	var res map[string]*Reply
+
+	for i := 0; i < len(ids); i++ {
+		iterStart := time.Now()
+		popped, err := client.BLPop(ctx, dur, ids...).Result()
+		if err != nil {
+			return nil, errs.Internalf("redis error on round %v: %w", i+1, err)
+		}
+		if len(popped) != 2 {
+			return nil, errs.Internalf("round %v: expected 2 results, got %v", i+1, popped)
+		}
+		var reply *Reply
+		err = json.Unmarshal([]byte(popped[1]), reply)
+		if err != nil {
+			return nil, errs.Internalf("json parse on round %v: %w", i+1, err)
+		}
+		res[popped[0]] = reply
+		dur = dur - time.Now().Sub(iterStart)
+		if dur <= 0 {
+			return res, errs.Internalf("request timed out")
+		}
+		ids = removeValueStrings(ids, popped[0])
+	}
+	return res, nil
+}
