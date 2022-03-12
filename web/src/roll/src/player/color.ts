c7f5ff7213a45fa59890aeea6545533eee20687d
S. B. Immington <shadowroller@immington.industries>
2022-03-12 16:37:12 -0500

    Remove unused file

--- web/src/roll/src/player/color.ts
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-
-import theme from 'theme';
-
-// ColorCtx is the player color that we use to accent parts of the UI.
-export const ColorCtx = React.createContext<string>(theme.colors.secondary); // TODO
